d6d6b5ea36422c15a5bac6a08462e0317805f158
HaileySanders82 <103207122+HaileySanders82@users.noreply.github.com>
2024-04-10 17:14:02 -0600

    CSP HEADER WOOOO

--- Program.cs
+++ Program.cs
@@ -70,6 +70,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.Use(async (context, next) => {
+        context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; img-src 'self' data: https://m.media-amazon.com https://www.lego.com https://images.brickset.com https://www.brickeconomy.com https://cdn.builder.io; script-src 'self' https://code.jquery.com https://cdn.jsdelivr.net https://maxcdn.bootstrapcdn.com; font-src 'self' https://fonts.gstatic.com https://fonts.googleapis.com; style-src 'self' https://maxcdn.bootstrapcdn.com https://cdn.jsdelivr.net 'unsafe-inline';"); // Adjust these directives according to your application's requirements
+        await next();
+    });
+
     app.UseMigrationsEndPoint();
 }
 else

d04113a75863edb84bcc74c9a4c6475f82ed005a
Johnathan Fietkau <ijohnadon@gmail.com>
2024-04-10 14:49:54 -0600

    component

---
+++ Components/CartSummaryViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Intextwo.Models;
+
+namespace Intextwo.Components
+{
+    public class CartSummaryViewComponent: ViewComponent
+    {
+        private Cart cart;
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            this.cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+
+    }
+}

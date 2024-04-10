742b5341def90ad93996884785fbade29d7d1c59
HaileySanders82 <103207122+HaileySanders82@users.noreply.github.com>
2024-04-10 14:07:39 -0600

    google auth 2

--- Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -62,7 +62,7 @@ namespace Intextwo.Areas.Identity.Pages.Account.Manage
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 12)]
             [DataType(DataType.Password)]
             [Display(Name = "New password")]
             public string NewPassword { get; set; }

a9e890563a8417df6dbe115dd865866c6911ad3f
Johnathan Fietkau <ijohnadon@gmail.com>
2024-04-11 18:43:41 -0600

    context

--- Models/LegoDbContext.cs
+++ Models/LegoDbContext.cs
@@ -13,20 +13,7 @@ namespace Intextwo.Models
         {
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // Configuration for Order and lineItem relationship
-            modelBuilder.Entity<Order>()
-                .HasMany(o => o.LineItems)
-                .WithOne(li => li.Order)
-                .HasForeignKey(li => li.transaction_ID);
 
-            // Primary key configuration for lineItem
-            modelBuilder.Entity<lineItem>()
-                .HasKey(li => new { li.transaction_ID, li.product_ID });
-
-            // ... Other configurations ...
-        }
 
         public virtual DbSet<Customer> customer { get; set; }
         public virtual DbSet<Product> products { get; set; }
@@ -35,5 +22,17 @@ namespace Intextwo.Models
         public virtual DbSet<ApplicationUser> AspNetUsers { get; set; }
         public virtual DbSet<UserCustomer> UserCustomer { get; set; }
         public virtual DbSet<Recommendation> recs { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configuring a composite key for lineItem
+            modelBuilder.Entity<lineItem>()
+                .HasKey(li => new { li.transaction_ID, li.product_ID });
+
+            // Additional configurations can be placed here
+        }
     }
 }
--- Models/lineItem.cs
+++ Models/lineItem.cs
@@ -5,11 +5,10 @@ namespace Intextwo.Models
 {
     public class lineItem
     {
-        [Key, Column(Order = 0)]
+        //[Key, Column(Order = 0)]
         public int transaction_ID { get; set; }
-        public virtual Order Order { get; set; } // Navigation property to Order
 
-        [Key, Column(Order = 1)]
+        //[Key, Column(Order = 1)]
         public int product_ID { get; set; }
         public int? qty { get; set; }
         public int? rating { get; set; }
